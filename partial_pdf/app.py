08b612537e244d3e77bfb2f880164e2b78dcb203
Nikolay Komolov <nkspb13@gmail.com>
2018-08-10 15:57:25 +0300

    Improve page checking

--- partial_pdf/app.py
+++ partial_pdf/app.py
@@ -9,26 +9,35 @@ import os, PyPDF2, easygui
 
 # Select a pdf file
 input_file_name = easygui.fileopenbox(title="Select a PDF file to open", default="*.pdf")
+# Exit app if user pressed Cancel
+if input_file_name is None:
+    exit()
+
+# Get page length of input file
+input_file = PyPDF2.PdfFileReader(open(input_file_name, "rb"))
+total_pages = input_file.getNumPages()
 
 # Choose a beginning page
 begin_page = easygui.enterbox("Enter the beginning page", "Beginning Page")
+if begin_page is None:
+    exit()
 # Check input
-while not begin_page.isdigit():
+while (not begin_page.isdigit() or begin_page == "0" or int(begin_page) > total_pages):
+    easygui.msgbox("Please provide a valid number", "Whoops!")
     begin_page = easygui.enterbox("Enter the beginning page", "Beginning Page")
-    
+    if begin_page is None:
+        exit()
+
 # Choose an ending page
 end_page = easygui.enterbox("Enter the ending page", "Ending Page")
+if end_page is None:
+    exit()
 # Check input
-while not end_page.isdigit():
+while (not end_page.isdigit() or end_page == "0" or int(end_page) > total_pages or int(end_page) < int(begin_page)):
+    easygui.msgbox("Please provide a valid number", "Whoops!")
     end_page = easygui.enterbox("Enter the ending page", "Ending Page")
-
-# Create a new pdf file
-input_file = PyPDF2.PdfFileReader(open(input_file_name, "rb"))
-output_PDF = PyPDF2.PdfFileWriter()
-
-for page_num in range(int(begin_page) - 1, int(end_page)):
-    page = input_file.getPage(page_num)
-    output_PDF.addPage(page)
+    if end_page is None:
+        exit()
 
 # Choose output file
 output_file_name = easygui.filesavebox(title="Save the PDF file as", default="*.pdf")
@@ -36,7 +45,14 @@ output_file_name = easygui.filesavebox(title="Save the PDF file as", default="*.
 while input_file_name == output_file_name:
     easygui.msgbox("Cannot overwrite the original file!", "Please choose another file...")
     output_file_name = easygui.filesavebox(title="Save the PDF file as", default="*.pdf")
-
+if output_file_name is None:
+    exit()
+    
+# Create a new pdf file
+output_PDF = PyPDF2.PdfFileWriter()
+for page_num in range(int(begin_page) - 1, int(end_page)):
+    page = input_file.getPage(page_num)
+    output_PDF.addPage(page)
 # Save output file
 output_file = open(output_file_name, "wb")
 output_PDF.write(output_file)
